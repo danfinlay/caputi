ee3da860949770e97624a5799eca584faf928bde
Dan Finlay <dan@danfinlay.com>
2020-06-18 00:10:43 -0700

    Object mode

--- example/bundle.js
+++ example/bundle.js
@@ -9661,7 +9661,7 @@ const makeCapTpFromStream = require('captp-stream');
 const websocket = require('websocket-stream')
 
 module.exports = function connectToAddress (address) {
-  const ws = websocket(address);
+  const ws = websocket(address, { objectMode: true });
   return makeCapTpFromStream('server', ws, {});
 }
 
