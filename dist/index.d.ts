64d9a2fb16f18f90b36c91e5807a3bac26ddfef4
Dan Finlay <dan@danfinlay.com>
2020-06-19 14:07:01 -0700

    Remove old build type

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { PropertyGenerator, ObservableGenerator, CaptpWsClientGenerator, CaptpWsServerGenerator } from './types';
-declare const generateProperties: PropertyGenerator;
-declare const observable: ObservableGenerator;
-declare const captpWsServer: CaptpWsServerGenerator;
-declare const captpWsClient: CaptpWsClientGenerator;
-export { generateProperties, observable, captpWsClient, captpWsServer };
