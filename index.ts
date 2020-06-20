984c2eda6112943e4a000516301110cd2b3c02f2
Dan Finlay <dan@danfinlay.com>
2020-06-20 15:39:04 -0700

    Export readonly grain function

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import { GrainGenerator, GrainMapGenerator, CaptpWsClientGenerator, CaptpWsServerGenerator } from './types';
+import readonly from './src/grain/types/readonly';
 
 const grainMap: GrainMapGenerator = require('./src/grain-map.js');
 const grain: GrainGenerator = require('./src/grain.ts');
 const createServer: CaptpWsServerGenerator = require('./src/captp-ws-server.js');
 const createClient: CaptpWsClientGenerator = require('./src/captp-ws-client.js');
 
-export { grainMap, grain, createServer, createClient };
+export { grainMap, grain, createServer, createClient, readonly };
+
